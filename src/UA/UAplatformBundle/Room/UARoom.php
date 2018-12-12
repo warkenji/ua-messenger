3b67c06b88b2638f9f9ee21b383a4611afed4a18
warkenji <warren.gsprd@gmail.com>
2018-12-12 15:27:07 -0400

    Ajout du chat

---
+++ src/UA/UAplatformBundle/Room/UARoom.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace UA\UAplatformBundle\Room;
+
+use Gos\Bundle\WebSocketBundle\Topic\TopicInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+use Gos\Bundle\WebSocketBundle\Router\WampRequest;
+
+class UARoom implements TopicInterface
+{
+    /**
+     * This will receive any Subscription requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param Topic $topic
+     * @param WampRequest $request
+     * @return void
+     */
+    public function onSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+        //this will broadcast the message to ALL subscribers of this topic.
+        $topic->broadcast(['data' => $connection->resourceId . " a rejoint le salon"]);
+    }
+
+    /**
+     * This will receive any UnSubscription requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param Topic $topic
+     * @param WampRequest $request
+     * @return voids
+     */
+    public function onUnSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+        //this will broadcast the message to ALL subscribers of this topic.
+        $topic->broadcast(['data' => $connection->resourceId . " a quitté le salon "]);
+    }
+
+    /**
+     * This will receive any Publish requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param $Topic topic
+     * @param WampRequest $request
+     * @param $data
+     * @param array $exclude
+     * @param array $eligibles
+     * @return mixed|void
+     */
+    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $data, array $exclude, array $eligible)
+    {
+        /*
+            $topic->getId() will contain the FULL requested uri, so you can proceed based on that
+
+            if ($topic->getId() == "acme/channel/shout")
+               //shout something to all subs.
+        */
+
+        $topic->broadcast([
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Like RPC is will use to prefix the channel
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ua.room';
+    }
+}
