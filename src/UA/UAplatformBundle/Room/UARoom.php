48f94658ce779b46e3c4ca0db03aa143c0ce412b
warkenji <warren.gsprd@gmail.com>
2018-12-12 21:16:28 -0400

    Mise en place des sessions d'utilisateurs

--- src/UA/UAplatformBundle/Room/UARoom.php
+++ src/UA/UAplatformBundle/Room/UARoom.php
@@ -3,12 +3,26 @@
 namespace UA\UAplatformBundle\Room;
 
 use Gos\Bundle\WebSocketBundle\Topic\TopicInterface;
+use Gos\Bundle\WebSocketBundle\Client\ClientManipulatorInterface;
 use Ratchet\ConnectionInterface;
 use Ratchet\Wamp\Topic;
 use Gos\Bundle\WebSocketBundle\Router\WampRequest;
 
 class UARoom implements TopicInterface
 {
+    /**
+     * @var ClientManipulatorInterface
+     */
+    protected $clientManipulator;
+
+    /**
+     * @param ClientManipulatorInterface $clientManipulator
+     */
+    public function __construct(ClientManipulatorInterface $clientManipulator)
+    {
+        $this->clientManipulator = $clientManipulator;
+    }
+
     /**
      * This will receive any Subscription requests for this topic.
      *
@@ -20,7 +34,8 @@ class UARoom implements TopicInterface
     public function onSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
     {
         //this will broadcast the message to ALL subscribers of this topic.
-        $topic->broadcast(['data' => $connection->resourceId . " a rejoint le salon"]);
+        $utilisateur = $this->clientManipulator->getClient($connection);
+        $topic->broadcast(['data' => $utilisateur->getUsername() . " a rejoint le salon"]);
     }
 
     /**
@@ -29,26 +44,27 @@ class UARoom implements TopicInterface
      * @param ConnectionInterface $connection
      * @param Topic $topic
      * @param WampRequest $request
-     * @return voids
+     * @return void
      */
     public function onUnSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
     {
         //this will broadcast the message to ALL subscribers of this topic.
-        $topic->broadcast(['data' => $connection->resourceId . " a quitté le salon "]);
+        $utilisateur = $this->clientManipulator->getClient($connection);
+        $topic->broadcast(['data' => $utilisateur->getUsername() . " a quitté le salon "]);
     }
 
     /**
      * This will receive any Publish requests for this topic.
      *
      * @param ConnectionInterface $connection
-     * @param $Topic topic
+     * @param Topic $topic
      * @param WampRequest $request
      * @param $data
      * @param array $exclude
      * @param array $eligibles
      * @return mixed|void
      */
-    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $data, array $exclude, array $eligible)
+    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $data, array $exclude, array $eligibles)
     {
         /*
             $topic->getId() will contain the FULL requested uri, so you can proceed based on that
@@ -57,9 +73,17 @@ class UARoom implements TopicInterface
                //shout something to all subs.
         */
 
+
+        $utilisateur = $this->clientManipulator->getClient($connection);
+        $userId = [$connection->WAMP->sessionId];
+
         $topic->broadcast([
-            'data' => $data
-        ]);
+            'data' => trim($utilisateur->getUsername()) . ": " . trim($data)
+        ], $userId);
+
+        $topic->broadcast([
+            'data' => "vous: " . trim($data)
+        ], [], $userId);
     }
 
     /**
